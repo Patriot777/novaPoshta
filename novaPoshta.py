72e7d046637bcb75ee279782ad65a061a62ba609
Володимир <zolinbest@gmail.com>
2020-07-02 11:07:29 +0300

    Add files via upload

---
+++ novaPoshta.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+track = input("Введіть свій трек код: ")
+#track = "20450244638521"
+userCode = "https://novaposhta.ua/tracking/?cargo_number=" + track
+site = requests.get(userCode)
+
+soup = BeautifulSoup(site.text, 'html.parser')
+
+DeliveryAddress = soup.find_all('tr')[2].text.strip()    #Адреса доставки
+Address = DeliveryAddress[15:]
+
+Rout = soup.find_all('tr')[1].text.strip()               #Маршрут
+rout = Rout[7:]
+
+DateOfArrival = soup.find_all('tr')[3].text.strip()      #Дата прибуття
+dateofArrival = DateOfArrival[13:]
+print("Маршрут: " + rout)
+print("Адреса доставки - " + Address)
+print("Дата доставки: " + dateofArrival)
+print("")
+exitProgram = input("Натисніть любу кнопку для виходу з програми...")
+#print(statusTime)
